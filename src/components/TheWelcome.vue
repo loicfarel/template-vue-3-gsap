1f20b00213e1ef79327f6c571e552b8175be2dfc
Loïc Farel <80321198+loicfarel@users.noreply.github.com>
2024-07-08 12:25:23 +0100

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center w-[100dvw] h-[100dvh] bg-[#1A1A1A]">
+    <h1 class="h1-start">Vue 3 Projet.</h1>
+  </div>
+</template>
+
+<style scoped>
+.h1-start {
+  background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  font-size: 60px;
+  font-weight: 700;
+}
+</style>
